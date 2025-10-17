684bfd3406731a5dabe289aa7fb435e6653b1032
Adam161378 <sadowskiadam79@gmail.com>
2025-10-17 10:00:55 +0200

    Clothing.java

--- Clothing.java
+++ Clothing.java
@@ -1,11 +1,5 @@
-//Klasa book dziedziczy z klasy Product
-public class Clothing extends Product implements Discountable { 
-public book(String name, double price, int id){
-    super(name, price, id);
-}
-
-@Override
-public void applyDiscount(){
-    price -= price * (percentage/100);  // obliczenie znizki
-}
+public class Clothing extends Product {
+    public Clothing(String name, double price, int id) {
+        super(name, price, id);
+    }
 }
