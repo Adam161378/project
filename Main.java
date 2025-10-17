9e5320caf20bb396b1bc07513f1636618fffd85f
Adam161378 <sadowskiadam79@gmail.com>
2025-10-17 10:00:11 +0200

    Main.java

--- Main.java
+++ Main.java
@@ -1,27 +1,24 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        // Lista produktów (koszyk)
         ArrayList<Product> cart = new ArrayList<>();
 
-        // Dodajemy produkty do koszyka
-        cart.add(new Product("Książka Java", 59.99, 1));
-        cart.add(new Product("Laptop", 2499.99, 2));
-        cart.add(new Product("Koszulka", 39.99, 3));
+        cart.add(new Book("Książka Java", 59.99, 1));
+        cart.add(new Electronics("Laptop", 2499.99, 2));
+        cart.add(new Clothing("Koszulka", 39.99, 3));
 
-        // Zniżka 10% na każdy produkt
         for (Product p : cart) {
-            p.applyDiscount(10); // zniżka 10%
+            p.applyDiscount(30);
         }
 
-        // Wyświetlenie koszyka
         double total = 0;
         System.out.println("Koszyk:");
         for (Product p : cart) {
-            p.display();            // wyświetl produkt
-            total += p.price;       // sumuj ceny
+            p.display();
+            total += p.getPrice();
         }
 
-        // Łączna cena
         System.out.printf("Suma do zapłaty: %.2f PLN\n", total);
     }
 }
